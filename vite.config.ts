7b71f2c0acde778bdd444e4dc862452bc5f2335b
Daniel Misic <daniel@misic.ca>
2025-11-22 07:49:01 -0500

    Fix white screen: Fix entry point and Google Client ID syntax

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/cleanswift/',   // <-- REQUIRED FOR GITHUB PAGES
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -21,3 +22,4 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+
